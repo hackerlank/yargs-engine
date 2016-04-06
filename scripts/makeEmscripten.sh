60497112a3e55c20edd2b8d8f6540d2e7ec6fd75
Marios Hadjimichael <marioshadj@gmail.com>
2016-04-06 16:37:16 -0700

    started working on emscripten build script.sh

---
+++ scripts/makeEmscripten.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#This script is supposed to be in the "scripts" directory
+#Go back to parent directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+cd ..
+
+#Activate "emscripten"
+
+# source Emscripten
+
+#Make temporary dir
+directory=/tmp/$RANDOM-$$-emscripten
+mkdir $directory
+
+# copy source in /tmp/<random-dir>
+cp -r "source/" "$directory"
+# copy assets in /tmp/<random-dir>
+cp -r "assets/" "$directory"
+
+# generate makefile from original makefile
+	# include all assets
+# cd /tmp/<random-dir>
+# emmake 
+# copy compiled files back
+# delete tmp?
+
+
