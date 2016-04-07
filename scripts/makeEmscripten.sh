6ea6dae65f76ba6a838858ad18788fd251a227dd
Marios Hadjimichael <marioshadj@gmail.com>
2016-04-06 21:30:12 -0700

    Completed the makeEmscripten.sh script

--- scripts/makeEmscripten.sh
+++ scripts/makeEmscripten.sh
@@ -7,8 +7,12 @@ cd "$DIR"
 cd ..
 
 #Activate "emscripten"
-./
+EMSCRDIR="EmscriptenSDK"
+
+"./$EMSCRDIR/emsdk" activate latest
+
 # source Emscripten
+source "$EMSCRDIR/emsdk_env.sh"
 
 #Make temporary dir
 directory=/tmp/$RANDOM-$$-emscripten
@@ -19,11 +23,27 @@ cp -r "source/" "$directory"
 # copy assets in /tmp/<random-dir>
 cp -r "assets/" "$directory"
 
-# generate makefile from original makefile
-	# include all assets
-# cd /tmp/<random-dir>
-# emmake 
-# copy compiled files back
-# delete tmp?
+#cd to tmp dir
+cd "$directory"
 
+# generate makefile from original makefile
+EMSCRIPTEN_MAKE_ARGS_AUTO=$(< "$directory/makefile_em")
+echo $EMSCRIPTEN_MAKE_ARGS_AUTO
+mv makefile makefile.original
+perl -pe 's/\#\<EMSCRIPTEN_FLAGS\>/`cat makefile_em`/e' makefile.original > makefile
+
+#Run emmake
+emmake make
+
+#Copy "executable file" to 
+#remove old executable:
+PROJECTDIR="$(dirname "$DIR")"
+#rm -fr "$PROJECTDIR/build_web" &
+mkdir -p "$PROJECTDIR/build_web" 
+mv {Game.html,Game.js,Game.data} "$PROJECTDIR/build_web/" && echo -e "\x1B[32mSuccess! The compiled files should be at build_web/\x1B[0m"
+
+exit
+
+#remove temporary directory
+rm -fr "$directory"
 
